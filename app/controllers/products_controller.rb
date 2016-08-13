bbab8424da075ebd903e557f1da84496ed72ac7e
C.J. Bordeleau <cj@cj.gg>
2016-08-13 19:40:05 -0400

    Fill in ProductsController actions.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,18 @@
 class ProductsController < ApplicationController
 
   def index
+    @products = Product.order(:created_at).limit(10)
+    respond_to do |format|
+      format.html
+      format.json {render json: @products}
+    end
   end
 
   def show
+    @product = Product.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json {render json: @product}
+    end
   end
 end
