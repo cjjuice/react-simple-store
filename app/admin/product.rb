60b581490dba995215448650c5ec38f10db5594c
C.J. Bordeleau <cj@cj.gg>
2016-08-13 19:24:30 -0400

    product admin resource tweaks.

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,13 +1,24 @@
 ActiveAdmin.register Product do
   menu priority: 1
-  permit_params :name, :price, :description
+  permit_params :name, :price, :description, :image
+
+  index do
+    column :name
+    column :price do |p|
+      number_to_currency p.price
+    end
+    column :image do |p|
+      image_tag(p.image.url(:thumb))
+    end
+    actions
+  end
 
   show do |product|
     attributes_table do
       row :name
       row :price
       row :image do
-        image_tag(ad.image.url(:thumb))
+        image_tag(product.image.url(:thumb))
       end
     end
   end
@@ -15,7 +26,7 @@ ActiveAdmin.register Product do
   form do |f|
     f.inputs "Product Info" do
       f.input :name
-      f.input :price
+      f.input :price, label: "Price ($)"
       f.input :description
       f.input :image, required: false
     end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { large: '1400x', medium: "800x", thumb: "100x" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
-  validates_presence_of :name, :description, :image
+  validates_presence_of :name, :price, :description
+  validates :image, attachment_presence: true
 end
